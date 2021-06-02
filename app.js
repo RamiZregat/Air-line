bce7be1c6810f046c9569a66de097aac5c0e771c
RamiZregat <>
2021-06-02 13:33:29 +0000

    modifying

---
+++ app.js
@@ -0,0 +1,6 @@
+var YourDestination = prompt("Where is your destination?")
+
+if(YourDestination="Paris"){confirm("You choiced Paris, its number 1 in list of travel destinations")}
+else if(YourDestination="london"){confirm("You choiced London, its number 2 in list of travel destinations")}
+else if(YourDestination="Rome"){confirm("You choiced Rome, its number 3 in list of travel destinations")}
+else if(YourDestination=null){confirm("We'r sorry we dont support this destination here")}
